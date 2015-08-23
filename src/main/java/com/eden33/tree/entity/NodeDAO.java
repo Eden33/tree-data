9e451ade368d98d2e392c9ad38b06fa0bb775348
Eden33 <eduard.gopp@gmail.com>
2015-08-23 21:46:54 +0200

    populate tree table with data on each web application startup

---
+++ src/main/java/com/eden33/tree/entity/NodeDAO.java
@@ -0,0 +1,51 @@
+package com.eden33.tree.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.Table;
+
+/**
+ * @author edi
+ */
+@NamedNativeQueries({
+    @NamedNativeQuery(
+        name = "generateTreeDataProcedure",
+        query = "CALL prc_fill_hierarchy(:level, :fill)"
+    )
+})
+@Entity
+@Table(name="t_hierarchy", indexes = {@Index(columnList = "id, parent")})
+public class NodeDAO {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(columnDefinition = "INT unsigned", nullable = false)
+    private int id;
+    
+    @Column(columnDefinition = "INT unsigned", nullable = false)
+    private int parent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+    
+    
+}
